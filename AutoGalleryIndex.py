9f4278066c6926f4e909dd0c412e047111f4d895
ion201 <njsimon@asu.edu>
2015-02-26 10:59:16 -0700

    Thumbnail images load (slowly)

--- AutoGalleryIndex.py
+++ AutoGalleryIndex.py
@@ -9,8 +9,9 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET'])
 def gallery():
     DOCROOT = '/var/www'
-
+    
     env_vars = {}
+
     env_vars['current_directory'] = DOCROOT + flask.request.script_root
     env_vars['autogalleryindex_version'] = '0.1.0'
 
@@ -19,7 +20,13 @@ def gallery():
             os.symlink(env_vars['current_directory'], env_vars['current_directory'] + '_static')
         return flask.redirect(flask.request.script_root + '_static/' + flask.request.args['dest'])
 
-    env_vars['dir_contents'] = sorted(os.listdir(env_vars['current_directory']))
+    items = sorted(os.listdir(env_vars['current_directory']))
+    env_vars['dir_contents'] = []
+    for item in items:
+        if os.path.isdir(env_vars['current_directory'] + '/' + item):
+            env_vars['dir_contents'].append((item, 'd'))  # Directory
+        else:
+            env_vars['dir_contents'].append((item, 'f'))  # File
 
     return flask.render_template('Gallery.html', **env_vars)
 
