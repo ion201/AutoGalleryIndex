19236195484d2d6c5235fd24405f54e52431d9e8
ion201 <njsimon@asu.edu>
2015-02-27 10:39:05 -0700

    Better thumbnails

--- AutoGalleryIndex.py
+++ AutoGalleryIndex.py
@@ -2,7 +2,7 @@
 
 import flask
 import os
-from PIL import Image
+from PIL import Image, ImageFilter
 
 app = flask.Flask(__name__)
 
@@ -27,10 +27,9 @@ def thumbnails(img_dir, thumb_dir):
             continue  # Don't thumbnail non images (duh)
         
         thumb_dest = '%s/%s' % (thumb_dir, file_name)
-        if not os.path.exists(thumb_dest):
-            thumb = Image.open(abs_path)
-            thumb.thumbnail((100, 178), Image.ANTIALIAS)
-            thumb.save(thumb_dest)
+        #if not os.path.exists(thumb_dest):
+        thumb = Image.open(abs_path).resize((100, 178), Image.ANTIALIAS).filter(ImageFilter.DETAIL)
+        thumb.save(thumb_dest)
 
 
 @app.route('/<path:subfolder>')
