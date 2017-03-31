1c62ec27c781410be29742bafa0f2b3e43ee6b89
ion201 <moniseat101@yahoo.com>
2017-03-31 18:20:15 -0400

    Added thumbnail creation (request blocking)

--- AutoGalleryIndex.py
+++ AutoGalleryIndex.py
@@ -3,8 +3,7 @@
 import flask
 import os
 from PIL import Image, ImageFilter
-import time
-import subprocess
+import hashlib
 
 import FileMimetypes as mime
 
@@ -19,13 +18,28 @@ app.config['ROW_ITEMS_LONG'] = 5
 app.config['EXCLUDE_HIDDEN_FILES'] = True
 app.config['MAX_LINE_CHARACTERS'] = 20
 app.config['MAX_LINES_PER_ENTRY'] = 3
+app.config['THUMB_MAX_WIDTH'] = 178
+app.config['THUMB_MAX_HEIGHT'] = 100
+app.config['THUMB_IMAGE_TYPE'] = '.jpg'
 
 log_message = print
 
 
-def get_cache_location():
-    return os.path.join(app.config['CACHE_HOME'], app.config['APPLICATION_NAME'])
+class ThumbnailError(RuntimeError):
+    pass
 
+
+def get_cache_location_abs():
+    return os.path.join(app.config['CACHE_ABS'], app.config['APPLICATION_NAME'])
+
+def get_cache_location_url():
+    abs_path = get_cache_location_abs()
+    app.config['DOCROOT'] = app.config['DOCROOT']
+    if app.config['DOCROOT'] not in abs_path:
+        raise ValueError('The cache path (%s) must be inside the docroot (%s)' % (
+                            app.config['DOCROOT'], abs_path))
+
+    return abs_path.replace(app.config['DOCROOT'], '')
 
 def is_mobile_request(request_headers):
     mobile_tags = ('Android', 'Windows Phone', 'iPod', 'iPhone')
@@ -58,10 +72,53 @@ def grants_write_permission(path):
     return False
 
 
+def get_cache_name_for_file(filepath):
+    mtime = os.path.getmtime(filepath)
+    basename = hashlib.sha1(('%s%f' % (filepath, mtime)).encode()).hexdigest()
+    return basename + app.config['THUMB_IMAGE_TYPE']
+
+
+def create_thumbnail(input_path, output_path):
+    """Create a thumbnail and save output in filesystem
+    output_path must be relative to cache home. Raises ThumbnailError on exceptions
+    and returns the path relative to webroot on success"""
+    if os.path.isabs(output_path):
+        raise OSError('Could not create thumbnail: Path (%s) is not a relative path' % (
+                                output_path,))
+
+    if not os.path.isfile(input_path):
+        raise ThumbnailError('The input path (%s) does not appear to be valid' % input_path)
+
+    # Assume that this path has already been validated
+    output_path_abs = os.path.join(get_cache_location_abs(), output_path)
+    output_path_url = os.path.join(get_cache_location_url(), output_path)
+
+    if not os.path.exists(output_path_abs):
+        # Don't overwrite a pre-existing file
+
+        try:
+            thumb = Image.open(input_path)
+            aspect_ratio = thumb.size[0] / thumb.size[1]
+            if app.config['THUMB_MAX_WIDTH'] / aspect_ratio > app.config['THUMB_MAX_HEIGHT']:
+                height = app.config['THUMB_MAX_HEIGHT']
+                width = int(app.config['THUMB_MAX_HEIGHT'] * aspect_ratio)
+            else:
+                width = app.config['THUMB_MAX_WIDTH']
+                height = int(app.config['THUMB_MAX_WIDTH'] / aspect_ratio)
+            thumb = thumb.resize((width, height), Image.ANTIALIAS).filter(ImageFilter.DETAIL)
+            thumb.save(output_path_abs)
+        except Exception as e:
+            # Lots of things could have gone wrong here. For now, just shove the information about
+            # them into a single easily-caught exception
+            raise ThumbnailError(repr(e))
+
+    return output_path_url
+
+
 @app.before_first_request
 def test_cache_directory():
     """Returns True if the cache is able to be used; Otherwise False"""
-    cache_dir_path = get_cache_location()
+    cache_dir_path = get_cache_location_abs()
     try:
         if not os.path.exists(cache_dir_path):
             os.makedirs(cache_dir_path)
@@ -119,6 +176,7 @@ def gallery(relpath):
         return flask.send_from_directory(*os.path.split(abs_path))
 
     template_vars['dir_contents'] = []
+
     directory_items = get_directory_contents(abs_path)
     for item in directory_items:
         item_abs_path = os.path.join(abs_path, item)
@@ -128,10 +186,21 @@ def gallery(relpath):
 
         file_type = mime.get_type(item_abs_path)
 
+        thumb_path = flask.url_for('static', filename=os.path.join('icons', file_type))
+        if file_type == mime.MIME_IMAGE_GENERIC:
+            thumb_cached_location = get_cache_name_for_file(item_abs_path)
+            try:
+                thumb_path = create_thumbnail(item_abs_path, thumb_cached_location)
+                file_type = mime.MIME_IMAGE_THUMBED
+
+            except ThumbnailError as e:
+                log_message('Thumb error: %s' % str(e))
+
+
         item_relpath = os.path.join(relpath, item)
 
         item = reformat_filename(item)
-        template_vars['dir_contents'].append((item, item_relpath, file_type))
+        template_vars['dir_contents'].append((item, item_relpath, file_type, thumb_path))
 
     template_vars['dir_contents'].sort(key=lambda x: '..%s' % x[0].lower() if x[2] == mime.MIME_DIRECTORY else x[0].lower())
     # Insert a special entry for the "back" button if applicable
@@ -161,5 +230,5 @@ def index():
 if __name__ == '__main__':
     # For debugging only
     app.config['DOCROOT'] = '/var/www/html/GalleryDemo'
-    app.config['CACHE_HOME'] = '/var/www/html/cache/'
+    app.config['CACHE_ABS'] = app.config['DOCROOT'] + '/cache'
     app.run(host='0.0.0.0', port=9001, debug=True)
